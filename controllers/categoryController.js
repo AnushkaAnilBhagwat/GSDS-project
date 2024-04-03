c433581f7df5716759b3cea15960ae28d32c4d1c
Anushka Bhagwat <anushkabhagwat3@gmail.com>
2024-04-03 11:28:47 +0530

    product categories API added

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -29,3 +29,77 @@ export const createCategoryController = async (req,res) => {
         })
     }
 };
+
+export const updateCategoryController = async(req,res) => {
+    try{
+        const {name} = req.body
+        const {id} = req.params
+        const category = await categoryModel.findByIdAndUpdate(id, {name, slug: slugify(name)}, {new:true})
+        res.status(200).send({
+            message: 'category updated successsfuly',
+            category,
+        })
+    }catch(error){
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            error,
+            message: "Error while updating category"
+        })
+    }
+}
+
+export const getCategoryController = async (req, res) => {
+    try{
+        const category = await categoryModel.find({});
+        res.status(200).send({
+            success: true,
+            message: "All categories list",
+            category,
+        });
+    }catch(error){
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            message: "Error while getting categories",
+            error,
+        })
+    }
+}
+
+export const getSingleCategoryController = async(req,res) => {
+    try {
+        const {id} = req.params
+        const category = await categoryModel.findById(id)
+        res.status(200).send({
+            success: true,
+            message: "Found the category",
+            category,
+        })
+    }catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success: false,
+            error,
+            message: "Error while getting the category"
+        })
+    }
+}
+
+export const deleteCategoryController = async (req, res) => {
+    try {
+        const {id} = req.params
+        await categoryModel.findByIdAndDelete(id)
+        res.status(200).send({
+            success: true,
+            message: "Successfuly Deleted the category",
+        })
+    }catch(error) {
+        console.log(error);
+        res.status(500).send({
+            success: false,
+            error,
+            message: "Error while deleting the category"
+        })
+    }
+}
--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -1,8 +1,16 @@
 import express from 'express'
-import { createCategoryController } from '../controllers/categoryController.js';
+import { createCategoryController, deleteCategoryController, getCategoryController, getSingleCategoryController, updateCategoryController } from '../controllers/categoryController.js';
 
 const router = express.Router()
 
-router.post('/create-category', createCategoryController)
+router.post('/create-category', createCategoryController);
+
+router.put('/update-category/:id', updateCategoryController);
+
+router.get('/get-category', getCategoryController);
+
+router.get('/get-single-category/:id', getSingleCategoryController);
+
+router.delete("/delete-category/:id", deleteCategoryController)
 
 export default router;
